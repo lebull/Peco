6f87ae61350e48c9ccf375e6353b5fd566643457
lebull <bulldogboy912000@yahoo.com>
2014-12-11 09:51:22 -0600

    Initial Commit
    Initial Commit

---
+++ client.py
@@ -0,0 +1,27 @@
+from twisted.internet.protocol import Protocol, ClientFactory
+from twisted.internet import reactor
+from sys import stdout
+
+HOST = 'localHOST'
+PORT = 8007
+
+class Echo(Protocol):
+    def dataReceived(self, data):
+        stdout.write(data)
+
+class EchoClientFactory(ClientFactory):
+    def startedConnecting(self, connector):
+        print 'Started to connect.'
+
+    def buildProtocol(self, addr):
+        print 'Connected.'
+        return Echo()
+
+    def clientConnectionLost(self, connector, reason):
+        print 'Lost connection.  Reason:', reason
+
+    def clientConnectionFailed(self, connector, reason):
+        print 'Connection failed. Reason:', reason
+
+reactor.connectTCP(HOST, PORT, EchoClientFactory())
+reactor.run()
